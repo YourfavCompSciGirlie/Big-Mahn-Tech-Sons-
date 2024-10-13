f7789337fbaec787b49f804d1bcee46ae8f3882a
Lebogang <lebomz@outlook.com>
2024-10-13 20:51:41 +0200

    Added command section that allows a citizen to travel across the map Stratgy mode of Transport needs to be checked more to that it takes account distance between current and destination and choose the appropriate mode of transport strategy.

---
+++ GoHome.h
@@ -0,0 +1,25 @@
+#ifndef GOHOME_H
+#define GOHOME_H
+
+#include "GoToCommand.h"
+#include "Citizen.h"
+#include "RoadNetWork.h"
+#include "Node.h"
+
+
+/**
+ * @class  GoHome
+ * @brief   This class is a concrete command that allows a citizen to travel from the current position to home
+ */
+
+
+class GoHome : public GoToCommand
+{
+   private:
+      Citizen* citizen;
+      Node* destination;
+      RoadNetWork* roadNetwork;
+   public:
+      GoHome(Citizen* citizen, RoadNetWork* roadNetwork);
+}
+#endif
---
+++ GoToEnt.h
@@ -0,0 +1,27 @@
+#ifndef GOTOENT_H
+#define GOTOENT_H
+
+#include "GoToCommand.h"
+#include "Citizen.h"
+#include "RoadNetWork.h"
+#include "Node.h"
+
+/**
+ * @class  GoToEnt
+ * @brief   This class is a concrete command that allows a citizen to travel from the current position to a random entertainment node
+ */
+
+
+class GoToEnt : public GoToCommand
+{
+   private:
+      Citizen* citizen;
+      Node* destination;
+      RoadNetWork* roadNetwork;
+
+   public:
+      GoToEnt(Citizen* citizen, RoadNetWork* roadNetwork);
+};
+
+
+#endif
